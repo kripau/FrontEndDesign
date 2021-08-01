cd236fa4708dfc7629326137d82617ba772a41c7
kripa <kripaupretis.com>
2021-08-01 21:49:37 +1000

    Landing page added in the front end project

---
+++ Landing_page/README.md
@@ -0,0 +1,13 @@
+# Assignment 2: Landing page
+
+## Project Summary
+In this project, we have created an interactive multi-section landing page where DOM is automatically updated using Javascript. Following functionalities are implemented:
+- Automatically generate the navigation bar associated with each section of the HTML body
+- Highlight the navigation bar when clicked to indicate the active section
+- Click action on the  navigation item at the top, scroll, and navigate to the corresponding section of the HTML page
+- Highlight the corresponding section of the HTML page to differentiate from the rest of the sections by assigning the active CSS class to the section on both click on the navigation bar or manual scroll action
+- Create a button on each section to scroll to the top of the page from each section
+- Improve the performance of loading by using fragments while creating new elements
+
+## Learning Scope
+This project combines the understanding of HTML, CSS, and JavaScript concepts together to implement the functionality. Besides, using these three technologies, we have improved performance as well.
---
+++ Landing_page/js/app.js
@@ -0,0 +1,148 @@
+/**
+ *
+ * Manipulating the DOM exercise.
+ * Exercise programmatically builds navigation,
+ * scrolls to anchors from navigation,
+ * and highlights section in viewport upon scrolling.
+ *
+ * Dependencies: None
+ *
+ * JS Version: ES2015/ES6
+ *
+ * JS Standard: ESlint
+ *
+ */
+
+/**
+ * Define Global Variables
+ *
+ */
+const sectionList = document.querySelectorAll("section");
+
+/**
+ * End Global Variables
+ * Start Helper Functions
+ *
+ */
+
+// creating nav anchor element
+function createNavElement(sec) {
+  let anchor = document.createElement("a");
+  anchor.className = "menu__link";
+  anchor.dataset.id = sec.getAttribute("id");
+  anchor.innerText = sec.getAttribute("data-nav");
+  return anchor;
+}
+
+// activate section
+function activateSection(id) {
+  for (let i = 0; i < sectionList.length; i++) {
+    if (sectionList[i].id === id) {
+      sectionList[i].classList.add("your-active-class");
+    } else {
+      sectionList[i].classList.remove("your-active-class");
+    }
+  }
+}
+
+//activate nav
+function activateNav(id) {
+  const navItems = document.getElementsByClassName("menu__link");
+  for (let i = 0; i < navItems.length; i++) {
+    if (navItems[i].dataset.id === id) {
+      navItems[i].classList.add("nav-active-class");
+    } else {
+      navItems[i].classList.remove("nav-active-class");
+    }
+  }
+}
+
+/**
+ * End Helper Functions
+ * Begin Main Functions
+ *
+ */
+
+// build the nav
+
+function addNavItem() {
+  let fragment = document.createDocumentFragment();
+  for (let i = 0; i < sectionList.length; i++) {
+    let navLi = document.createElement("li");
+    const anchorElement = createNavElement(sectionList[i]);
+    navLi.appendChild(anchorElement);
+    fragment.appendChild(navLi);
+  }
+  let ulElement = document.getElementById("navbar__list");
+  ulElement.appendChild(fragment);
+}
+
+/**
+ * End Main Functions
+ * Begin Events
+ *
+ */
+
+// nav  click eventhandler
+function enableScroll() {
+  const navItems = document.getElementsByClassName("menu__link");
+  for (let i = 0; i < navItems.length; i++) {
+    navItems[i].addEventListener("click", function (event) {
+      const sectionID = event.target.getAttribute("data-id");
+      const section = document.getElementById(sectionID);
+      activateNav(sectionID);
+      section.scrollIntoView();
+      activateSection(sectionID);
+    });
+  }
+}
+
+// mouse enter evenhandler
+// highlight Nav and Section area
+function highlightSectionNav() {
+  for (let i = 0; i < sectionList.length; i++) {
+    let functionHandler = function (i) {
+      return function () {
+        id = sectionList[i].getAttribute("id");
+        activateSection(id);
+        activateNav(id);
+      };
+    };
+    sectionList[i].addEventListener("mouseenter", functionHandler(i));
+  }
+}
+
+// Build menu (done)
+
+// Scroll to section on link click (done)
+
+// Set sections as active (done)
+
+// create button which when clicked scrolls to the top of the page
+
+function buttonElement() {
+  let divPosition = document.getElementsByClassName("landing__container");
+  for (let i = 0; i < divPosition.length; i++) {
+    let button = document.createElement("BUTTON");
+    const buttonText = document.createTextNode("TOP");
+    button.appendChild(buttonText);
+    divPosition[i].appendChild(button);
+    button.addEventListener("click", function () {
+      window.scroll({
+        top: 0,
+        left: 0,
+        behavior: "smooth",
+      });
+    });
+  }
+}
+
+// Call all the required function
+function main() {
+  addNavItem();
+  enableScroll();
+  highlightSectionNav();
+  buttonElement();
+}
+
+main();
